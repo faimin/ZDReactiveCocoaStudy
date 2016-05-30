b296afdbe52c05a0c987ac5a68558571f8664e37
符现超 <fuxianchao@gmail.com>
2016-05-30 20:22:58 +0800

    update

--- ReactiveCocoaDemo/RACController.m
+++ ReactiveCocoaDemo/RACController.m
@@ -4,8 +4,11 @@
 //
 //  Created by 符现超 on 15/8/22.
 //  Copyright (c) 2015年 zd. All rights reserved.
-//  信号演示网站：http://neilpa.me/rac-marbles/
-//  http://www.tuicool.com/articles/Yrim6zZ
+//  信号演示网站：
+//  http://rxmarbles.com/
+//  http://neilpa.me/rac-marbles/
+
+//  http://www.tuicool.com/articles/Yrim6zZ Reactive Cocoa 学习笔记
 
 #import "RACController.h"
 #import "PushController.h"
@@ -59,7 +62,7 @@
 
     //[self concat];
     
-    [self then];
+    //[self then];
 
     //[self replay];
 
@@ -71,6 +74,7 @@
     
     //[self throttle];
     
+    [self collect];
 }
 
 #pragma mark - Functions
@@ -87,7 +91,6 @@
 /// 将一组Signal发出的最新的事件合并成一个Signal，每当这组Signal发出新事件时，reduce的block会执行，将所有新事件的值合并成一个值，并当做合并后Signal的事件发出去。
 /// 这个方法会返回合并后的Signal。
 /// reduce的block中参数，其实是与combineLatest中数组元素一一对应的
-/// 注意：
 - (void)combineLatestReduce
 {
     RACSignal *signalA = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
@@ -125,16 +128,15 @@
         NSArray *resultArray = seque.array;
         LxDBAnyVar(resultArray);
         
-        RACTupleUnpack(NSString * str1, NSString * str2) = x;
+        RACTupleUnpack(NSString *str1, NSString *str2) = x;
         NSString *str = FORMATSTRING(@"%@%@", str1, str2);
         LxPrintf(@"%@,\n %@", x, str);
     }];
 }
 
-/**
- *  把多个信号合并成一个信号，任何一个信号有新值的时候就会调用
- *  和combine reduce差不多,总是返回最新的信号
- */
+///  把多个信号合并成一个信号，任何一个信号有新值的时候就会调用，它会按照时间的先后顺序把信号排列起来
+///  和combine reduce差不多,总是返回最新的信号，
+///  区别：每次merge返回的是单一的信号，不能组合，而combineLatest可以把最新返回的信号跟另一个信号进行组合，另一个信号是它的上次的那个信号。可以简单理解成combineLatest可以组合，而merge却不行
 - (void)merge
 {
     RACSignal *signalA = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
@@ -156,17 +158,15 @@
     }];
 }
 
-/**
- *  @brief  压缩: 把两个信号中的数据连到一起，生成一个RACTuple，[A
- * zipWith:B]中A和B至少发送过一次消息，zip才会把他们打包成一个tuple
- *
- *  比如下面的demo，如果打开signalB中的sendNext:@"4",
- *  则会输出2次，输出结果分别为13，24，如果不打开则只输出一个结果：13，相当于两个信号中的数据会一一映射，当对应的数据缺失时则不执行压缩操作
- 
- *  意味着如果是一个流的第N个元素，一定要等到另外一个流第N值也收到才会一起组合发出。
- *
- *  [C zipWith:D]可以比喻成一对平等恩爱的夫妻，两个人是“绑在一起“的关系来组成一个家庭，决定一件事（value）时必须两个人都提出意见（当且仅当C和D同时都产生了值的时候，一个value才被输出，C、D只有其中一个有值时会挂起，等待另一个的值，所以输出都是一对值（RACTuple）），当夫妻只要一个人先挂了（completed）这个家庭（组合起来的RACStream）就宣布解散（也就是无法凑成一对输出时就终止）
- */
+///  @brief  压缩: 把两个信号中的数据连到一起，生成一个RACTuple，[A
+/// zipWith:B]中A和B至少发送过一次消息，zip才会把他们打包成一个tuple
+///
+///  比如下面的demo，如果打开signalB中的sendNext:@"4",
+///  则会输出2次，输出结果分别为13，24，如果不打开则只输出一个结果：13，相当于两个信号中的数据会一一映射，当对应的数据缺失时则不执行压缩操作
+///
+///  意味着如果是一个流的第N个元素，一定要等到另外一个流第N值也收到才会一起组合发出。
+///
+///  [C zipWith:D]可以比喻成一对平等恩爱的夫妻，两个人是“绑在一起“的关系来组成一个家庭，决定一件事（value）时必须两个人都提出意见（当且仅当C和D同时都产生了值的时候，一个value才被输出，C、D只有其中一个有值时会挂起，等待另一个的值，所以输出都是一对值（RACTuple）），当夫妻只要一个人先挂了（completed）这个家庭（组合起来的RACStream）就宣布解散（也就是无法凑成一对输出时就终止）
 - (void)zip
 {
     RACSignal *signalB = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
@@ -185,21 +185,19 @@
     }];
 
     [[signalB zipWith:signalA] subscribeNext:^(id x) {
-        RACTupleUnpack(NSString * str1, NSString * str2) = x;
+        RACTupleUnpack(NSString *str1, NSString *str2) = x;
         NSString *str = FORMATSTRING(@"%@%@", str1, str2);
         LxPrintf(@"%@,\n %@", x, str);
     }];
 }
 
-/**
- *  @brief  连接：把一个信号传递的数据拼接到另一个信号的数据上
- *  返回一个新的数组。该数组是通过把所有 arrayX 参数添加到 arrayObject 中生成的。如果要进行 concat() 操作的参数是数组，那么添加的是数组中的元素，而不是数组。
- *
- *  [A concat:B]中只有A信号执行完complete之后才会执行B，如果A失败（比如执行了sendError），则B永远不会执行
- *  A和B像皇上和太子的关系，A是皇上，B是太子。皇上健在的时候统治天下发号施令（value），太子就候着，不发号施令（value），当皇上挂了（completed），太子登基当皇上，此时发出的号令（value）是太子的。
- *
- *  下面的返回结果是ABCDEFGHI123456789
- */
+/// @brief  连接
+/// 把一个信号传递的数据拼接到另一个信号的数据后面
+/// 返回一个新的数组，该数组是通过把所有 arrayX 参数添加到 arrayObject 中生成的。如果要进行 concat() 操作的参数是数组，那么添加的是数组中的元素，而不是数组。
+///
+/// [A concat:B]中只有A信号执行完complete之后才会执行B，如果A失败（比如执行了sendError），则B永远不会执行
+///
+/// A和B像皇上和太子的关系，A是皇上，B是太子。皇上健在的时候统治天下发号施令（value），太子就候着，不发号施令（value），当皇上挂了（completed），太子登基当皇上，此时发出的号令（value）是太子的。
 - (void)concat
 {
     RACSequence *letters =
@@ -209,6 +207,7 @@
 
     RACSequence *concat = [letters concat:numbers];
     LxDBAnyVar([concat array]);
+    //print: ABCDEFGHI123456789
 }
 
 /// 当一个订阅者被发送了completed事件后，then:方法才会执行，订阅者会订阅then:方法返回的Signal，这个Signal是在block中返回的。
@@ -237,14 +236,12 @@
     }];
 }
 
-/**
- *  重新运行：当有新的订阅者时信号会重新发送以前发送过的数据给这个订阅者
- *  下面的例子中，如果不加replay，则第二个订阅者不会受到它上面已经发送过的信号（即s2不执行，不打印任何数据），加上replay后它会收以前已经发送过的A和B两个数据。
- *  replayLast会把最后一个信号数据发给后来的订阅者，如下，s2只会输出B
- *  replayLazily
- * 不会重新执行,他与其他2个不同之处是RAC中的信号只有在被订阅后才会执行，而其他的只要执行到那就会执行，就跟do-while一样
- *  http://spin.atomicobject.com/2014/06/29/replay-replaylast-replaylazily/
- */
+/// 重新运行：当有新的订阅者时信号会重新发送以前发送过的数据给这个订阅者
+/// 下面的例子中，如果不加replay，则第二个订阅者不会受到它上面已经发送过的信号（即s2不执行，不打印任何数据），加上replay后它会收以前已经发送过的A和B两个数据。
+/// replayLast会把最后一个信号数据发给后来的订阅者，如下，s2只会输出B
+/// replayLazily
+/// 不会重新执行,他与其他2个不同之处是RAC中的信号只有在被订阅后才会执行，而其他的只要执行到那就会执行，就跟do-while一样
+/// http://spin.atomicobject.com/2014/06/29/replay-replaylast-replaylazily/
 - (void)replay
 {
     ///// RACSubject是RACSignal的子类，它可以快速创建一个信号，用来发送信息
@@ -294,9 +291,7 @@
     }];
 }
 
-/**
- *  replay或者replayLazily只会让信号里面只发送一次，即只执一次num++，当有订阅者时就只会拿到信号当初发送的数据，不会重新发送新的，如果不用replay，则每次有订阅者都会导致racsignal执行一次
- */
+/// replay或者replayLazily只会让信号里面只发送一次，即只执一次num++，当有订阅者时就只会拿到信号当初发送的数据，不会重新发送新的，如果不用replay，则每次有订阅者都会导致racsignal执行一次
 - (void)replayLazily
 {
     __block int num = 0;
@@ -320,16 +315,12 @@
     }];
 }
 
-/**
- *  参数为RACScheduler类的对象scheduler，这个方法会返回一个Signal，它的所有事件都会传递给scheduler参数所表示的线程，而以前管道上的副作用还会在以前的线程上。这个方法主要是切换线程。
- */
+///  参数为RACScheduler类的对象scheduler，这个方法会返回一个Signal，它的所有事件都会传递给scheduler参数所表示的线程，而以前管道上的副作用还会在以前的线程上。这个方法主要是切换线程。
 - (void)deliverOn
 {
 }
 
-/**
- *  比较数值流中当前值和上一个值，如果不同，就返回当前值，简单理解为“流”的值有变化时反馈变化的值，求异存同。它将这一次的值与上一次做比较，当相同时（也包括-isEqual:）被忽略掉。
- */
+///  比较数值流中当前值和上一个值，如果不同，就返回当前值，简单理解为“流”的值有变化时反馈变化的值，求异存同。它将这一次的值与上一次做比较，当相同时（也包括-isEqual:）被忽略掉。
 - (void)distinctUntilChanged
 {
     [[self.textField.rac_textSignal distinctUntilChanged] subscribeNext:^(id x) {
@@ -341,11 +332,9 @@
 
 // http://blog.sunnyxx.com/2014/04/19/rac_4_filters/
 
-/**
- *  忽略给定的值，注意，这里忽略的既可以是地址相同的对象，也可以是-
- * isEqual:结果相同的值，也就是说自己写的Model对象可以通过重写-
- * isEqual:方法来使- ignore:生效。
- */
+///  忽略给定的值，注意，这里忽略的既可以是地址相同的对象，也可以是-
+/// isEqual:结果相同的值，也就是说自己写的Model对象可以通过重写-
+/// isEqual:方法来使- ignore:生效。
 - (void)ignore
 {
     [[self.textField.rac_textSignal ignore:@"123"] subscribeCompleted:^{
@@ -353,27 +342,18 @@
     }];
 }
 
-/**
- *  这个比较极端，忽略所有值，只关心Signal结束，也就是只取Comletion和Error两个消息，中间所有值都丢弃。
- *  注意，这个操作应该出现在Signal有终止条件的的情况下，如rac_textSignal这样除dealloc外没有终止条件的Signal上就不太可能用到。
- */
+///  这个比较极端，忽略所有值，只关心Signal结束，也就是只取Comletion和Error两个消息，中间所有值都丢弃。
+///  注意，这个操作应该出现在Signal有终止条件的的情况下，如rac_textSignal这样除dealloc外没有终止条件的Signal上就不太可能用到。
 - (void)ignoreValues
 {
 }
 
-/**
- *  @brief
- * 跳过：它会把第三条数据之前的所有数据都跳过，然后只输出第四条极其后面的数据，下面的输出结果为4，5（一次输出一条数据）
- *
- *
- *  - skipUntilBlock:(BOOL (^)(id x))
- *
- *  和- takeUntilBlock:同理，一直跳，直到block为YES
- *
- *  - skipWhileBlock:(BOOL (^)(id x))
- *
- *  和- takeWhileBlock:同理，一直跳，直到block为NO
- */
+///  @brief
+/// 跳过：它会把第三条数据之前的所有数据都跳过，然后只输出第四条极其后面的数据，下面的输出结果为4，5（一次输出一条数据）
+///  - skipUntilBlock:(BOOL (^)(id x))
+///  和- takeUntilBlock:同理，一直跳，直到block为YES
+///  - skipWhileBlock:(BOOL (^)(id x))
+///  和- takeWhileBlock:同理，一直跳，直到block为NO
 - (void)skip
 {
     RACSignal *signalA = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
@@ -401,9 +381,7 @@
     }];
 }
 
-/**
- *  take与skip相反，它是取take的前几条数据
- */
+/// take与skip相反，它是取take的前几条数据
 - (void)take
 {
     RACSignal *signal =
@@ -467,9 +445,8 @@
 //
 //   上面的反向逻辑，对于每个next值，block返回YES时才取值
 
-/**
-*  take数据，直到xxx时候停止
-*/
+
+/// take数据，直到xxx时候停止
 - (void)takeUntil
 {
     [[self.textField.rac_textSignal takeUntilBlock:^BOOL(NSString *text) {
@@ -562,7 +539,7 @@
 }
 
 
-///  当signalA和signalB都至少sendNext过一次，接下来只要其中任意一个signal有了新的内容，doA:withB这个方法就会自动被触发。
+/// 当signalA和signalB都至少sendNext过一次，接下来只要其中任意一个signal有了新的内容，doA:withB这个方法就会自动被触发。
 - (void)liftSelector
 {
     RACSignal *signalA = [RACSignal createSignal:^RACDisposable *(id<RACSubscriber> subscriber) {
@@ -580,6 +557,26 @@
     }];
     
     [self rac_liftSelector:@selector(doA:withB:) withSignals:signalA, signalB, nil];
+}
+
+/// collect 操作会把多个信号中所有的 next 发送的数据收集到一个 NSArray 中，然后一次性通过 next 发送给后续的环节。
+/// http://fengjian0106.github.io/2016/04/28/The-Power-Of-Composition-In-FRP-Part-3/
+- (void)collect
+{
+    RACSignal *numbers = @[@(0), @(1), @(2)].rac_sequence.signal;
+    
+    RACSignal *letters1 = @[@"A", @"B", @"C"].rac_sequence.signal;
+    RACSignal *letters2 = @[@"X", @"Y", @"Z"].rac_sequence.signal;
+    RACSignal *letters3 = @[@"M", @"N"].rac_sequence.signal;
+    NSArray *arrayOfSignal = @[letters1, letters2, letters3]; //2
+    
+    [[[numbers map:^id(NSNumber *value) {
+        return arrayOfSignal[value.integerValue];
+    }] collect] subscribeNext:^(NSArray *array) {
+        NSLog(@"%@, %@", [array class], array);
+    } completed:^{
+        NSLog(@"completed");
+    }];
 }
 
 #pragma mark - Search
